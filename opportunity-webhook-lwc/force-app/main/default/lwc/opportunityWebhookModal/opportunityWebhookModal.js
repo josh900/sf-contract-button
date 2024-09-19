2453a76b184bcfb74afff6ca4dc30263651070db
josh900 <jm5522@sbcglobal.net>
2024-09-19 14:36:01 -0700

    Update opportunityWebhookModal.js

--- opportunity-webhook-lwc/force-app/main/default/lwc/opportunityWebhookModal/opportunityWebhookModal.js
+++ opportunity-webhook-lwc/force-app/main/default/lwc/opportunityWebhookModal/opportunityWebhookModal.js
@@ -23,24 +23,16 @@ export default class OpportunityWebhookModal extends LightningModal {
     }
 
     connectedCallback() {
-        // console.log('OpportunityWebhookModal connected');
-        // console.log('AccountId:', this.accountId);
-        // console.log('OpportunityId:', this.opportunityId);
-        // console.log('OpportunityFields:', JSON.stringify(this.opportunityFields));
         this.fetchWebhookData();
     }
 
     async fetchWebhookData() {
-        // console.log('fetchWebhookData called');
         try {
             console.log('Calling getWebhookData with opportunityId:', this.opportunityId);
             const result = await getWebhookData({ 
                 id: this.opportunityId, 
                 isOpportunityId: true 
             });
-            // console.log('getWebhookData result:', result);
-            // console.log('Raw webhook response:', result);
-            // console.log('Webhook data received:', result);
             if (result) {
                 try {
                     const parser = new DOMParser();
@@ -55,7 +47,6 @@ export default class OpportunityWebhookModal extends LightningModal {
                 } catch (e) {
                     this.htmlContent = result;
                 }
-                // console.log('Processed webhook data:', this.htmlContent);
                 this.isLoading = false;
             } else {
                 throw new Error('No data received from webhook');
@@ -76,6 +67,7 @@ export default class OpportunityWebhookModal extends LightningModal {
         console.log('Submitting contract');
         this.isSubmitting = true;
         try {
+            console.log('Sending to webhook with opportunityId:', this.opportunityId);
             const result = await sendToWebhook({ opportunityId: this.opportunityId });
             console.log('Webhook submission result:', result);
             this.close('Contract submitted');
@@ -88,12 +80,10 @@ export default class OpportunityWebhookModal extends LightningModal {
     }
 
     handleClose() {
-        // console.log('Closing modal');
         this.close('Modal closed');
     }
 
     renderedCallback() {
-        // console.log('Modal rendered. HTML content:', this.htmlContent);
         if (this.htmlContent) {
             const container = this.template.querySelector('.webhook-content');
             if (container) {
